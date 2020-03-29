e96805e91887c9fe9bcd049eee2d275f4b063cb6
Matthew Corfiatis <matt.corf@gmail.com>
2020-03-29 20:24:40 +1300

    Implement MParallelSorter3 using fork join library

--- src/ass1/MParallelSorter2.java
+++ src/ass1/MParallelSorter2.java
@@ -8,16 +8,15 @@ public class MParallelSorter2 implements Sorter {
 
     @Override
     public <T extends Comparable<? super T>> List<T> sort(List<T> list) {
-        return MParallelSorter1.get(sortAsync(list));
+        return sortAsync(list).join();
     }
 
     private <T extends Comparable<? super T>> CompletableFuture<List<T>> sortAsync(final List<T> list) {
         if (list == null)
             return CompletableFuture.supplyAsync(() -> Collections.emptyList());
 
-        if (list.size() < 20) {
+        if (list.size() < 20)
             return CompletableFuture.supplyAsync(() -> MSequentialSorter.doSort(list));
-        }
 
         int midIndex = list.size() / 2;
         final List<T> firstHalf = list.subList(0, midIndex);
--- src/ass1/MParallelSorter3.java
+++ src/ass1/MParallelSorter3.java
@@ -1,12 +1,43 @@
 package ass1;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveTask;
 
-public class MParallelSorter3 implements Sorter {
+public class MParallelSorter3<T extends Comparable<? super T>> extends RecursiveTask<List<T>> implements Sorter {
+    public static final ForkJoinPool pool = new ForkJoinPool();
+    private final List<T> list;
 
-  @Override
-  public <T extends Comparable<? super T>> List<T> sort(List<T> list) {
-    return null;
-  }
+    public MParallelSorter3() {
+        list = null;
+    }
 
+    public MParallelSorter3(List<T> list) {
+        this.list = list;
+    }
+
+    @Override
+    public <T extends Comparable<? super T>> List<T> sort(List<T> list) {
+        return pool.invoke(new MParallelSorter3<>(list));
+    }
+
+    @Override
+    protected List<T> compute() {
+        if (list == null)
+            return Collections.emptyList();
+
+        if (list.size() < 20)
+            return MSequentialSorter.doSort(list);
+
+        int midIndex = list.size() / 2;
+        List<T> firstHalf = list.subList(0, midIndex);
+        List<T> secondHalf = list.subList(midIndex, list.size());
+
+        MParallelSorter3<T> sortedFirstHalf = new MParallelSorter3<>(firstHalf);
+        MParallelSorter3<T> sortedSecondHalf = new MParallelSorter3<>(secondHalf);
+        invokeAll(sortedFirstHalf, sortedSecondHalf);
+
+        return MSequentialSorter.merge(sortedFirstHalf.join(), sortedSecondHalf.join());
+    }
 }
